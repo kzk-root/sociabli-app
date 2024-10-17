d9087cf2586757afdcd9f2dc4513420d7d0e8f2e
Maurice Renck <maurice@maurice-renck.de>
2024-10-17 13:49:23 +0200

    feat: wip get token

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,6 +1,38 @@
 import { Link } from 'react-router-dom'
+import { useAuth } from '@clerk/clerk-react'
+import { useEffect, useState } from 'react'
 
 export default function DashboardPage() {
+  const { getToken } = useAuth()
+  const [token, setToken] = useState<string>('')
+
+  useEffect(() => {
+      const fetchData = async () => {
+        const token = await getToken()
+
+        if(!token) return
+
+        setToken(token)
+      }
+
+      fetchData()
+  }, [getToken, setToken])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = 'http://localhost:3000/api/protected' // FIXME
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            // "x-access-token": "lass_mich_rein_hack",
+            Authorization: `Bearer ${token}`,
+          },
+        }).then((res) => res.json())
+    }
+
+    fetchData()
+  }, [token])
+
   return (
     <>
       <h1>Dashboard page</h1>
