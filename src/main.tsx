297ffabf350d718029e592663e6b73eb648eb2a8
Mark Schmeiser <mark.schmeiser@konzentrik.de>
2024-11-11 13:40:47 +0100

    fix: remove obsolete store

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import log from 'loglevel'
-import { StoreProvider } from 'easy-peasy'
-import store from '@/store'
 
 // FIXME set based on env var
 log.setLevel('debug')
 log.debug('Starting app')
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <StoreProvider store={store}>
     <App />
-  </StoreProvider>
 )
