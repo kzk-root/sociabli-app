6e2e853a24e28b7d843fd5ae76a26e8fef03d87a
Maurice Renck <maurice@maurice-renck.de>
2025-08-05 10:24:30 +0200

    feat: rss feed

--- blog/.eleventy.js
+++ blog/.eleventy.js
@@ -12,24 +12,13 @@ module.exports = function (eleventyConfig) {
     return DateTime.fromJSDate(value).toFormat(format)
   })
 
-  eleventyConfig.addPlugin(feedPlugin, {
-    type: 'rss', // or "atom", "json"
-    outputPath: '/blog/feed.rss',
-    collection: {
-      name: 'posts', // your collection name
-      limit: 10, // number of items in the feed
-    },
-    metadata: {
-      language: 'en',
-      title: 'Sociabli Blog',
-      subtitle:
-        'Sociabli helps you to share your content online. Post once and let Sociabli sync to other services and platforms.',
-      base: 'https://sociab.li/',
-      author: {
-        name: 'Maurice Renck',
-      },
-    },
+  eleventyConfig.addFilter('toRFC3339', (dateInput) => {
+    // Handles JS Date object or date string
+    return DateTime.fromJSDate(
+      typeof dateInput === 'string' ? new Date(dateInput) : dateInput
+    ).toISO() // ISO 8601 matches RFC 3339
   })
+  // eleventyConfig.addPlugin(feedPlugin)
 
   return {
     dir: {
